ac6cd7db41df61d0c58d5c4a1e54a0d2047140b8
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-03-19 08:39:57 +0100

    Add project files.

---
+++ 16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tablica tab = new Tablica();
+            try
+            {
+                int value = tab.getElement(20);
+                Console.WriteLine("Element nr 20 ma wartość: " + value);
+            }
+            catch(IndexOutOfRangeException)
+            {
+                Console.WriteLine("Nie ma elementu o numerze 20!");
+            }
+        }
+    }
+}
---
+++ 16/Tablica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _16
+{
+    class Tablica
+    {
+        int[] tab;
+        public Tablica()
+        {
+            tab = new int[5];
+        }
+        public int getElement(int index)
+        {
+            return tab[index];
+        }
+        
+
+    }
+}
